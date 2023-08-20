e62c0243f48f076d693da82ebb26187715d9a852
HYUcoolguy <jin5378@hanyang.ac.kr>
2023-08-21 00:42:14 +0900

    [Setting] remove useless code(HelloWorld)

--- src/main/kotlin/com/data/handler/adapter/incoming/HelloService.kt
+++
@@ -1,9 +0,0 @@
-package com.data.handler.adapter.incoming
-
-import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
-
-@Service
-class HelloService {
-    fun hello() = Mono.just("Hello Webflux with Kotlin").log()
-}
